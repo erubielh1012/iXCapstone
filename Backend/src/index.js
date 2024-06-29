0b293b6ba519f9fc43275df8a4480c11532e37f1
erubielh1012 <erhu2729@colorado.edu>
2024-06-29 17:53:15 +0200

    run build works

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,14 +14,6 @@ const path = require("path")
 
 const app = express();
 
-// Serve Frontend from Express Backend ------------------------------
-app.use(express.static(path.join(__dirname, "../../Frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(
-    path.resolve(__dirname, "..", "..", "Frontend", "build", "index.html")
-  )
-);
-
 // Cross-origin resource sharing is a mechanism that allows a web page to access restricted resources from a server on a domain different than the domain that served the web page.
 // Enabling CORS for any unknown origin
 app.use(cors());
@@ -36,3 +28,11 @@ app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.listen(port, () => {
     console.log(`IX blogging app listening on port ${port}`)
 });
+
+// Serve Frontend from Express Backend ------------------------------
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, "..", "..", "frontend", "build", "index.html")
+  )
+);
